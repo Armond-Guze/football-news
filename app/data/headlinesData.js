47e014832fc97037db63277289233b34160d370b
Armond-Guze <Armond.Guze@yahoo.com>
2025-06-10 00:56:07 -0400

    took away ar

--- app/data/headlinesData.js
+++ app/data/headlinesData.js
@@ -1,18 +1,5 @@
 const headlines = [
-  {
-    title: "NFL: Aaron Rodgers’ contract details with Steelers",
-    slug: "aaron-rodgers-contract",
-    videoUrl: "https://www.youtube.com/watch?v=p5UVt9-wSLg",
-    summary:
-      "Insider sources reveal new terms on Rodgers’ future in Pittsburgh.",
-    content: `
-      After a surprising trade to the Steelers, new details have emerged about Aaron Rodgers’ multi-year deal.
-      According to NFL insiders, the contract includes a team option in 2026, a large signing bonus, and incentives tied to playoff wins.
-      Pittsburgh is investing heavily to build a competitive team around Rodgers, aiming to reshape the AFC landscape.
-      Analysts predict that Rodgers' arrival could spark a new era for the Steelers, energizing the fanbase and drawing attention across the league.
-      Stay tuned as we track how this high-profile move impacts team dynamics and league standings.
-    `,
-  },
+ 
   {
     title: "Jaxson Dart Impresses in minicamp",
     slug: "jaxson-dart-minicamp",
@@ -26,6 +13,20 @@ const headlines = [
       Follow our updates as we provide the latest news on his health and impact on the Grizzlies’ prospects.
     `,
   },
+  //  {
+  //   title: "NFL: Aaron Rodgers’ contract details with Steelers",
+  //   slug: "aaron-rodgers-contract",
+  //   videoUrl: "https://www.youtube.com/watch?v=p5UVt9-wSLg",
+  //   summary:
+  //     "Insider sources reveal new terms on Rodgers’ future in Pittsburgh.",
+  //   content: `
+  //     After a surprising trade to the Steelers, new details have emerged about Aaron Rodgers’ multi-year deal.
+  //     According to NFL insiders, the contract includes a team option in 2026, a large signing bonus, and incentives tied to playoff wins.
+  //     Pittsburgh is investing heavily to build a competitive team around Rodgers, aiming to reshape the AFC landscape.
+  //     Analysts predict that Rodgers' arrival could spark a new era for the Steelers, energizing the fanbase and drawing attention across the league.
+  //     Stay tuned as we track how this high-profile move impacts team dynamics and league standings.
+  //   `,
+  // },
 ];
 
 export default headlines;
