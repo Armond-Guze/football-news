dccb09db0812a2da3f0f894e330861eba2c23550
Armond-Guze <Armond.Guze@yahoo.com>
2025-06-24 01:09:32 -0400

    finished typescript cnoversion

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@
         port: '',
         pathname: '/images/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/vi/**',
+      },
     ],
   },
 };
