fc89758ed00afd98a6eee19e910fdb2f3ad0b19c
Armond-Guze <Armond.Guze@yahoo.com>
2025-06-28 10:37:33 -0400

    Remove all test files and jsconfig.json - cleanup for production build

---
+++ sanity/schema-clean.ts
@@ -0,0 +1,12 @@
+import powerRanking from "./schemaTypes/powerRanking";
+import headlineType from "./schemaTypes/headlineType";
+import tagType from "./schemaTypes/tagType";
+import { authorType } from "./schemaTypes/authorType";
+
+export const schemaTypes = [
+  powerRanking,
+  headlineType,
+  tagType,
+  authorType
+  // add other schemas here as needed
+];
